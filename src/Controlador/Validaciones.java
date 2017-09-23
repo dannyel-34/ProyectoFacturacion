a311fb56c49752a4c0acdc90679dd8639f7a8a41
Daniellecaalle <daniellecalle@hotmail.com>
2017-09-23 17:33:19 -0500

    Adiciono archivos del proyecto facturacion

---
+++ src/Controlador/Validaciones.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import com.sun.glass.events.KeyEvent;
+import javax.swing.JOptionPane;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Validaciones {
+
+    public void solotexto(char letra, java.awt.event.KeyEvent evt) {
+        if ((letra < 'a' || letra > 'z') && (letra < 'A' || letra > 'Z') && letra != KeyEvent.VK_BACKSPACE && letra != ' ') {
+            JOptionPane.showMessageDialog(null, "Solo se permiten letras!!!", "Error", JOptionPane.ERROR_MESSAGE);
+            evt.consume();
+
+        }
+
+    }
+
+    public void solonumeros(char num, java.awt.event.KeyEvent evt) {
+        if ((num < '0' || num > '9') && (num != KeyEvent.VK_BACKSPACE || num == ' ')) {
+            JOptionPane.showMessageDialog(null, "Solo se permiten números!!!", "Error", JOptionPane.ERROR_MESSAGE);
+            evt.consume();
+
+        }
+
+    }
+
+    public boolean validarEmail(String mail) {
+        
+        boolean b = false;
+        
+        String emailPattern="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(mail);
+        
+        if(matcher.matches()){
+            b =  true;
+        }
+        else{
+            b = false;
+        }
+        
+        return b;
+    }
+    
+    public void validarTelefono(char letra, java.awt.event.KeyEvent evt, int num){
+        if(num<7){
+            solonumeros(letra, evt);
+        }else if(num>=7&&num<10){
+            JOptionPane.showMessageDialog(null, "Por favor no ingrese mas digitos. Telefono es correcto", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+  
+    
+}
---
+++ src/proyectofacturacion/ProyectoFacturacion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofacturacion;
+
+import Vista.VistaMenu;
+import javax.swing.JFrame;
+import org.jvnet.substance.SubstanceLookAndFeel;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoFacturacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        SubstanceLookAndFeel.setSkin("org.jvnet.substance.skin.SubstanceMangoLookAndFeel");
+        SubstanceLookAndFeel.setCurrentTheme("org.jvnet.substance.theme.SubstanceAquaTheme");
+        VistaMenu menu = new VistaMenu();
+        menu.setVisible(true);
+    }
+    
+}
